9d491bd44a5c3a28d19f179c6455e38ec327f0fe
Hussein891 <hussinhadid@gmail.com>
2025-05-01 18:27:42 +0200

    CustomTextField

--- lib/widgets/add_note_bottomsheet.dart
+++ lib/widgets/add_note_bottomsheet.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custom_text_field.dart';
 
 class AddNoteBottomsheet extends StatelessWidget {
   const AddNoteBottomsheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(children: [
+        SizedBox(
+          height: 24,
+        ),
+        CustomTextField(),
+      ]),
+    );
   }
 }
---
+++ lib/widgets/custom_text_field.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/const.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorColor: kPreimColor,
+      decoration: InputDecoration(
+        hintText: 'Title',
+        helperStyle: TextStyle(color: kPreimColor),
+        border: buildBorder(),
+        enabledBorder: buildBorder(),
+        focusedBorder: buildBorder(kPreimColor),
+      ),
+    );
+  }
+
+  OutlineInputBorder buildBorder([color]) {
+    return OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+        borderSide: BorderSide(color: color ?? Colors.white));
+  }
+}
